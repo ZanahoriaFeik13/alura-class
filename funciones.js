c3587cddb53661350e15f8d8a8d24d4a9095d711
zanahoria <ca572753@gmail>
2023-05-06 17:23:59 -0500

    solucionador1er

---
+++ funciones.js
@@ -0,0 +1,74 @@
+
+//aqui tomamos damos entrada y salida de lo que se encriptara y desencripto
+const textArea= document.querySelector(".text-area");
+const mensaje= document.querySelector(".mensaje");
+
+//La letra "e" es convertida para "enter"
+//La letra "i" es convertida para "imes"
+//La letra "a" es convertida para "ai"
+//La letra "o" es convertida para "ober"
+//La letra "u" es convertida para "ufat"
+
+//botones para el html y llamar las funciones
+function encriptarbtn(){
+
+    const textoEncriptado = encriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value="";
+    mensaje.style.backgroundImage="none";
+
+
+}
+
+function desencriptarbtn(){
+
+    const textoEncriptado = desencriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value="";
+
+
+}
+
+//funcion para encriptar
+function encriptar(stringEncriptada){
+let matrizCodigo = [["e","enter",],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    stringEncriptada= stringEncriptada.toLowerCase();
+
+
+    for(let i=0; i<matrizCodigo.length; i++){
+
+        if (stringEncriptada.includes(matrizCodigo[i][0])){
+
+            stringEncriptada=stringEncriptada.replaceAll (matrizCodigo[i][0],matrizCodigo[i][1])
+
+
+
+        }
+
+
+    }
+    return stringEncriptada;
+}
+
+//funcion para desencriptar
+function desencriptar(stringDesenncriptada){
+    let matrizCodigo = [["e","enter",],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+        stringDesenncriptada= stringDesenncriptada.toLowerCase();
+
+        for(let i =0; i< matrizCodigo.length;i++)
+        
+        if(stringDesenncriptada.includes(matrizCodigo[i][1])){
+
+            stringDesenncriptada=stringDesenncriptada.replaceAll(matrizCodigo[i][1],matrizCodigo[i][0])
+
+
+        }
+        return stringDesenncriptada;
+
+
+
+
+}
+
+
+
